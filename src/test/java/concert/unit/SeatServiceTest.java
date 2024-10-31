6f9ff1079a48618165fdbefc7c87045fed537b77
jjuya21 <jjuya21@gmail.com>
2024-11-01 06:25:47 +0900

    refactor: 테스트 용이성을 위한 수정

--- src/test/java/concert/unit/SeatServiceTest.java
+++ src/test/java/concert/unit/SeatServiceTest.java
@@ -24,6 +24,15 @@ class SeatServiceTest {
     private final long DEFAULT_SEAD_NO = 123L;
     private final long DEFAULT_CONCERT_ITEM_ID = 1L;
     private final long DEFAULT_PRICE = 80000L;
+    private final Seat DEFAULT_SEAT = Seat.builder()
+            .id(DEFAULT_ID)
+            .seatNo(DEFAULT_SEAD_NO)
+            .concertItemId(DEFAULT_CONCERT_ITEM_ID)
+            .price(DEFAULT_PRICE)
+            .status(SeatStatus.EMPTY)
+            .holdExpiryTime(null)
+            .build();
+
     @Mock
     private SeatRepository seatRepository;
     @InjectMocks
@@ -32,35 +41,23 @@ class SeatServiceTest {
     @Test
     void getSeat() throws Exception {
         // given
-        Seat savedSeat = Seat.builder().id(DEFAULT_ID).build();
-
         SeatInfo seatInfo = SeatInfo.builder()
                 .seatId(DEFAULT_ID)
                 .build();
 
-        given(seatRepository.getById(DEFAULT_ID)).willReturn(Optional.ofNullable(savedSeat));
+        given(seatRepository.getById(DEFAULT_ID)).willReturn(Optional.ofNullable(DEFAULT_SEAT));
 
         // when
         Seat result = seatService.getSeat(seatInfo);
 
         // then
         assertThat(result).isNotNull();
-        assertThat(result).isEqualTo(savedSeat);
         assertThat(result.getId()).isEqualTo(DEFAULT_ID);
     }
 
     @Test
     void updateStatusTest() throws Exception {
         // given
-        Seat savedSeat = Seat.builder()
-                .id(DEFAULT_ID)
-                .seatNo(DEFAULT_SEAD_NO)
-                .concertItemId(DEFAULT_CONCERT_ITEM_ID)
-                .price(DEFAULT_PRICE)
-                .status(SeatStatus.EMPTY)
-                .holdExpiryTime(null)
-                .build();
-
         Seat updateSeat = Seat.builder()
                 .id(DEFAULT_ID)
                 .seatNo(DEFAULT_SEAD_NO)
@@ -74,7 +71,7 @@ class SeatServiceTest {
                 .seatId(DEFAULT_ID)
                 .build();
 
-        given(seatService.getSeat(seatInfo)).willReturn(savedSeat);
+        given(seatRepository.getById(DEFAULT_ID)).willReturn(Optional.ofNullable(DEFAULT_SEAT));
         given(seatRepository.update(any(Seat.class))).willReturn(updateSeat);
 
         // when
@@ -85,5 +82,4 @@ class SeatServiceTest {
         assertThat(result.getId()).isEqualTo(DEFAULT_ID);
         assertThat(result.getStatus()).isEqualTo(SeatStatus.HOLDING);
     }
-
 }

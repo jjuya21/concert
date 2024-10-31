435a7cd95c9a0d660b183c4fa413a93f0d5f9651
jjuya21 <jjuya21@gmail.com>
2024-11-01 06:25:20 +0900

    refactor: 낙관적 락 삭제

--- src/main/java/concert/infrastructure/seat/SeatJpaRepository.java
+++ src/main/java/concert/infrastructure/seat/SeatJpaRepository.java
@@ -1,9 +1,7 @@
 package concert.infrastructure.seat;
 
 import concert.domain.seat.Seat;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +12,6 @@ public interface SeatJpaRepository extends JpaRepository<Seat, Long> {
 
     Seat save(Seat seat);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    // @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Seat> findById(long seatId);
 }

5b5fcd6abe204ca19c254508fcbf37baf0ff0874
jjuya21 <jjuya21@gmail.com>
2024-11-01 06:15:16 +0900

    feat: 결제에 대한 통합 테스트 작성

---
+++ src/test/java/concert/intergration/BalanceUseServiceTest.java
@@ -0,0 +1,105 @@
+package concert.intergration;
+
+import concert.application.chargebalance.ChargeBalance;
+import concert.domain.balance.Balance;
+import concert.domain.balance.BalanceRepository;
+import concert.domain.balance.service.BalanceInfo;
+import concert.domain.balance.service.BalanceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class BalanceUseServiceTest {
+
+    private final long DEFAULT_USER_ID = 1L;
+    private final long DEFAULT_AMOUNT = 200L;
+    private final long DEFAULT_BALANCE = 800000L;
+    @Autowired
+    private ChargeBalance chargeBalance;
+    @Autowired
+    private BalanceService balanceService;
+    @Autowired
+    private BalanceRepository balanceRepository;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        // createBalance();
+    }
+
+    @DisplayName("잔액 충전을 하면 amount만큼 금액이 충전되있어야한다")
+    @Test
+    @Transactional
+    void useBalanceTest() throws Exception {
+        // Given
+        BalanceInfo info = new BalanceInfo(DEFAULT_USER_ID, DEFAULT_AMOUNT);
+
+        // When
+        Balance actualBalance = balanceService.useBalance(info);
+
+        // Then
+        Balance updatedBalance = balanceService.getBalance(
+                BalanceInfo.builder()
+                        .userId(DEFAULT_USER_ID)
+                        .build()
+        );
+
+        assertThat(actualBalance.getUserId()).isEqualTo(DEFAULT_USER_ID);
+        assertThat(updatedBalance.getBalance()).isEqualTo(DEFAULT_BALANCE - DEFAULT_AMOUNT);
+    }
+
+    @Test
+    void useConcurrencyTest() throws Exception {
+        // given
+        createBalance();
+        int threadCount = 1000;
+
+        BalanceInfo info = new BalanceInfo(DEFAULT_USER_ID, DEFAULT_AMOUNT);
+
+        // when
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 1; i <= threadCount; i++) {
+            executor.execute(() -> {
+                try {
+                    balanceService.useBalance(info);
+                } catch (Exception ignored) {
+
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executor.shutdown();
+
+        // then
+        Balance updatedBalance = balanceService.getBalance(
+                BalanceInfo.builder()
+                        .userId(DEFAULT_USER_ID)
+                        .build()
+        );
+
+        assertThat(updatedBalance.getUserId()).isEqualTo(DEFAULT_USER_ID);
+        assertThat(updatedBalance.getBalance()).isEqualTo(DEFAULT_BALANCE - (DEFAULT_AMOUNT));
+    }
+
+    private void createBalance() {
+        balanceRepository.save(
+                Balance.builder()
+                        .userId(DEFAULT_USER_ID)
+                        .balance(DEFAULT_BALANCE)
+                        .build()
+        );
+    }
+}

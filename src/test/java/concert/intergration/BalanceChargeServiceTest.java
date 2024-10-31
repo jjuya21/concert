24739f2cf63f78f7553e512fece9eb8a6e757c0f
jjuya21 <jjuya21@gmail.com>
2024-11-01 06:18:23 +0900

    refactor: 락들의 비교를 위한 코드로 변경

--- src/test/java/concert/intergration/BalanceChargeServiceTest.java
+++ src/test/java/concert/intergration/BalanceChargeServiceTest.java
@@ -2,12 +2,10 @@ package concert.intergration;
 
 import concert.application.chargebalance.ChargeBalance;
 import concert.application.chargebalance.ChargeBalanceCommand;
-import concert.application.createtoken.CreateToken;
 import concert.domain.balance.Balance;
+import concert.domain.balance.BalanceRepository;
 import concert.domain.balance.service.BalanceInfo;
 import concert.domain.balance.service.BalanceService;
-import concert.domain.queuetoken.TokenStatus;
-import concert.domain.queuetoken.service.QueueTokenInfo;
 import concert.domain.queuetoken.service.QueueTokenService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,39 +14,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class BalanceChargeServiceTest {
 
+    private final long DEFAULT_USER_ID = 1L;
+    private final long DEFAULT_AMOUNT = 200L;
+    private final long DEFAULT_BALANCE = 800000L;
     @Autowired
     private ChargeBalance chargeBalance;
-
-    @Autowired
-    private CreateToken createToken;
-
     @Autowired
     private QueueTokenService queueTokenService;
-
     @Autowired
     private BalanceService balanceService;
-
-    private String token;
-    private long userId;
-    private long amount;
+    @Autowired
+    private BalanceRepository balanceRepository;
 
     @BeforeEach
     void setUp() throws Exception {
-
-        token = createToken.createToken().getToken();
-        queueTokenService.updateStatus(
-                QueueTokenInfo.builder()
-                        .token(token)
-                        .status(TokenStatus.PROCESSED)
-                        .build()
-        );
-        userId = 1L;
-        amount = 200L;
+//        createBalance();
     }
 
     @DisplayName("잔액 충전을 하면 amount만큼 금액이 충전되있어야한다")
@@ -56,7 +45,7 @@ class BalanceChargeServiceTest {
     @Transactional
     void chargeBalanceTest() throws Exception {
         // Given
-        ChargeBalanceCommand command = new ChargeBalanceCommand(userId, amount);
+        ChargeBalanceCommand command = new ChargeBalanceCommand(DEFAULT_USER_ID, DEFAULT_AMOUNT);
 
         // When
         Balance actualBalance = chargeBalance.chargeBalance(command);
@@ -64,12 +53,57 @@ class BalanceChargeServiceTest {
         // Then
         Balance updatedBalance = balanceService.getBalance(
                 BalanceInfo.builder()
-                        .userId(userId)
+                        .userId(DEFAULT_USER_ID)
                         .build()
         );
 
-        assertThat(updatedBalance.getBalance()).isEqualTo(amount);
-        assertThat(actualBalance.getUserId()).isEqualTo(userId);
-        assertThat(actualBalance.getBalance()).isEqualTo(amount);
+        assertThat(actualBalance.getUserId()).isEqualTo(DEFAULT_USER_ID);
+        assertThat(updatedBalance.getBalance()).isEqualTo(DEFAULT_BALANCE + DEFAULT_AMOUNT);
+    }
+
+    @Test
+    void chargeConcurrencyTest() throws Exception {
+        // given
+        createBalance();
+        int threadCount = 1000;
+
+        ChargeBalanceCommand command = new ChargeBalanceCommand(DEFAULT_USER_ID, DEFAULT_AMOUNT);
+
+        // when
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 1; i <= threadCount; i++) {
+            executor.execute(() -> {
+                try {
+                    chargeBalance.chargeBalance(command);
+                } catch (Exception ignored) {
+
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executor.shutdown();
+
+        // then
+        Balance updatedBalance = balanceService.getBalance(
+                BalanceInfo.builder()
+                        .userId(DEFAULT_USER_ID)
+                        .build()
+        );
+
+        assertThat(updatedBalance.getUserId()).isEqualTo(DEFAULT_USER_ID);
+        assertThat(updatedBalance.getBalance()).isEqualTo(DEFAULT_BALANCE + (DEFAULT_AMOUNT * threadCount));
+    }
+
+    private void createBalance() {
+        balanceRepository.save(
+                Balance.builder()
+                        .userId(DEFAULT_USER_ID)
+                        .balance(DEFAULT_BALANCE)
+                        .build()
+        );
     }
 }

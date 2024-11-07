556b0aebd8e78f000458f472b835cbb90842085b
jjuya21 <jjuya21@gmail.com>
2024-11-08 08:48:28 +0900

    refactor: 어노테이션 명을 RedisLock ->  LockManager으로 변경

--- src/main/java/concert/aop/LockManager.java
+++ src/main/java/concert/aop/LockManager.java
@@ -7,10 +7,10 @@ import java.lang.annotation.Target;
 
 @Target(value = ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RedisLock {
+public @interface LockManager {
     String key(); // 락을 걸 때 사용할 키 값
 
-    long waitTime() default 10; // 락 대기 시간 (초)
+    long waitTime() default 3; // 락 대기 시간 (초)
 
-    long leaseTime() default 30; // 락 유지 시간 (초)
+    long leaseTime() default 5; // 락 유지 시간 (초)
 }

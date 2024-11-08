6c0198c1e729665b7ff5a57ffc9df1bc32acbf46
jjuya21 <jjuya21@gmail.com>
2024-11-08 09:34:33 +0900

    refactor: RedisLock 어노테이션을 LockManager로 변경 및 관련 코드 수정

--- src/main/java/concert/interfaces/common/redis/RedissonConfig.java
+++ src/main/java/concert/interfaces/common/redis/RedissonConfig.java
@@ -6,7 +6,6 @@ import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RedissonConfig {
 
